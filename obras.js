cb82c5ba984938dcf4d7c8b0e972802a4226ad31
gjorgefrancisco98-afk <g.jorgefrancisco98@gmail.com>
2026-01-05 10:42:28 -0300

    Add files via upload

---
+++ obras.js
@@ -0,0 +1,14 @@
+/* BANCO DE DADOS ATUALIZADO EM 21/12/2025 */
+
+const BANCO_DE_OBRAS = [
+    {
+        "id": 1,
+        "nome": "UBS Florabella",
+        "contrato": "019/2025",
+        "empresa": "TM8 CONSTRUTORA EIRELI",
+        "objeto": "CONTRATAÇÃO DE EMPRESA ESPECIALIZADA PARA EXECUÇÃO DE SERVIÇOS DE CONSTRUÇÃO DE UMA UNIDADE BÁSICA DE SAÚDE (UBS)",
+        "inicio": "2025-04-07",
+        "termino": "2026-04-06"
+    },
+    
+];
